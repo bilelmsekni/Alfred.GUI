29056dd8f6b0e6608e93ac686eb5a03e4eedc916
SkiBLE <msekni.bilel@gmail.com>
2016-11-19 09:26:38 +0100

    updated code climate.yml

--- src/app/artifact/artifact.component.ts
+++ src/app/artifact/artifact.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { ArtifactModel } from './artifact.model';
-import { ArtifactService } from './artifact.service';
-import { IArtifact } from './artifact.entity';
+import { Component, OnInit } from "@angular/core";
+import { ArtifactModel } from "./artifact.model";
+import { ArtifactService } from "./artifact.service";
 
 @Component({
-    templateUrl: './artifact/artifact.component.html',
+    templateUrl: "./artifact/artifact.component.html",
     providers: [ArtifactService]
 })
 export class ArtifactComponent implements OnInit {
     model: ArtifactModel;
-    listFilter: string = '';
+    listFilter: string = "";
     constructor(private _artifactService: ArtifactService) {
         this.model = new ArtifactModel();
     }
