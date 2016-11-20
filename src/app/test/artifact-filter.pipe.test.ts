ef421dd5ac2dea81f49a82f8cf3a1b19ab1bb11e
SkiBLE <msekni.bilel@gmail.com>
2016-11-20 08:21:16 +0100

    Added More tests

--- src/app/test/artifact-filter.pipe.test.ts
+++ src/app/test/artifact-filter.pipe.test.ts
@@ -11,14 +11,32 @@ function createArtifact(id: number, title?: string): Artifact {
 
 describe("filter artifacts", function () {
     let artifacts = [
-        createArtifact(0, "formation java"),
+        createArtifact(0, "12a13 java"),
         createArtifact(0, "article bigdata"),
         createArtifact(0, "12a13 agile"),
         createArtifact(0),
     ];
-    it("should return all artifacts when filter doesnt match", function () {
+    it("should return all artifacts when filter is BDD", function () {
         let artifactFilter = new ArtifactFilterPipe();
         let res = artifactFilter.transform(artifacts, "BDD");
         expect(res.length).eql(0);
+    });
+
+    it("should return one artifact when filter is bigdata ", function () {
+        let artifactFilter = new ArtifactFilterPipe();
+        let res = artifactFilter.transform(artifacts, "bigdata");
+        expect(res.length).eql(1);
+    });
+
+    it("should return two artifacts when filter is 12a13 ", function () {
+        let artifactFilter = new ArtifactFilterPipe();
+        let res = artifactFilter.transform(artifacts, "12a13");
+        expect(res.length).eql(2);
+    });
+
+    it("should return one artifacts when filter is AGILE ", function () {
+        let artifactFilter = new ArtifactFilterPipe();
+        let res = artifactFilter.transform(artifacts, "AGILE");
+        expect(res.length).eql(1);
     });
 });
