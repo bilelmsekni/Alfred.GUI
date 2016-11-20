f2e2f9753b284e597384b38bad291813761cb2ec
SkiBLE <msekni.bilel@gmail.com>
2016-11-20 09:20:06 +0100

    Added tests for leader filter

--- src/app/member/leader-filter.pipe.ts
+++ src/app/member/leader-filter.pipe.ts
@@ -1,14 +1,24 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { Member } from './member.entity';
-import { CommunityRole } from './community-role.enum';
+import { Pipe, PipeTransform } from "@angular/core";
+import { Member } from "./member.entity";
+import { CommunityRole } from "./community-role.enum";
 
 @Pipe({
-    name: 'leaderFilter'
+    name: "leaderFilter"
 })
 export class LeaderFilterPipe implements PipeTransform {
 
-    transform(members: Member[]): string {
-        let leader = members.find((member: Member) => member.role == CommunityRole.Leader);
-        return leader ? leader.firstName + ' ' + leader.lastName : 'no leader';
+    public transform(members: Member[]): string {
+        let leader = members.find((member: Member) => this.isMatch(member));
+        return leader ? this.getLeaderFullName(leader) : "no leader";
+    }
+
+    private isMatch(member: Member): boolean {
+        return member.role === CommunityRole.Leader;
+    }
+
+    private getLeaderFullName(leader: Member): string {
+        return (leader.firstName ? leader.firstName : "")
+            + (leader.firstName && leader.lastName ? " " : "")
+            + (leader.lastName ? leader.lastName : "");
     }
 }
---
+++ src/app/test/leader-filter.pipe.test.ts
@@ -0,0 +1,45 @@
+import { expect } from "chai";
+import { LeaderFilterPipe } from "../member/leader-filter.pipe";
+import { Member } from "../member/member.entity";
+import { CommunityRole } from "../member/community-role.enum";
+
+function createMember(id: number, firstName?: string, lastName?: string, role?: CommunityRole): Member {
+    let member = new Member();
+    member.id = id;
+    member.firstName = firstName;
+    member.lastName = lastName;
+    member.role = role;
+    return member;
+}
+
+describe("find leader", function () {
+    let memberswithoutleader = [
+        createMember(0, "jack", "khan", CommunityRole.Member),
+        createMember(0, "jet", "go", CommunityRole.Manager),
+        createMember(0),
+    ];
+
+    let memberswithleader = memberswithoutleader.concat(
+        createMember(0, "tom", "jetcrose", CommunityRole.Leader));
+
+    let memberswithleaderwithNoLastName = memberswithoutleader.concat(
+        createMember(0, "tom", null, CommunityRole.Leader));
+
+    it("should return no leaders when no leader exists", function () {
+        let leaderFilter = new LeaderFilterPipe();
+        let res = leaderFilter.transform(memberswithoutleader);
+        expect(res).eql("no leader");
+    });
+
+    it("should return tom jetcrose when tom jetcrose is leader ", function () {
+        let leaderFilter = new LeaderFilterPipe();
+        let res = leaderFilter.transform(memberswithleader);
+        expect(res).eql("tom jetcrose");
+    });
+
+    it("should return tom when tom is leader ", function () {
+        let leaderFilter = new LeaderFilterPipe();
+        let res = leaderFilter.transform(memberswithleaderwithNoLastName);
+        expect(res).eql("tom");
+    });
+});
