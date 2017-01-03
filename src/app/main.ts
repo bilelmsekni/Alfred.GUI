a8133044a9a995d36246f858507faf0b0fad6be9
SkiBLE <msekni.bilel@gmail.com>
2017-01-03 07:27:37 +0100

    DEV | testing treeshaking and aot

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,14 @@
-import { platformBrowserDynamic  } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+// import { platformBrowserDynamic  } from '@angular/platform-browser-dynamic';
+// import { AppModule } from './app.module';
 import { enableProdMode } from '@angular/core';
 
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
 
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+// const platform = platformBrowserDynamic();
+// platform.bootstrapModule(AppModule);
+
+//import { platformBrowser }    from '@angular/platform-browser';
+// import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
+// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
