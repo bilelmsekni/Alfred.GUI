c9131a437faeed76e255d90358bfa501eae52926
SkiBLE <msekni.bilel@gmail.com>
2017-01-06 07:17:29 +0100

    fixed issue with generation aot

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,14 +1,10 @@
-// import { platformBrowserDynamic  } from '@angular/platform-browser-dynamic';
-// import { AppModule } from './app.module';
+import { platformBrowserDynamic  } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
 import { enableProdMode } from '@angular/core';
 
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
 
-// const platform = platformBrowserDynamic();
-// platform.bootstrapModule(AppModule);
-
-//import { platformBrowser }    from '@angular/platform-browser';
-// import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
-// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
