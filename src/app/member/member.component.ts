c7c833a694f63cfa99e6cea61db111a886d25039
SkiBLE <msekni.bilel@gmail.com>
2017-01-22 11:26:41 +0100

    DEV | Added first call to api

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
-@Component({
-    template : '{{helloWorld}} from memberComponent'
-})
-export class MemberComponent {
-    public helloWorld: string = 'Hello Ng2 BBL';
+import { Member } from './member.entity';
+import { Http, Response } from '@angular/http';
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+    templateUrl: './member.component.html'
+})
+export class MemberComponent implements OnInit {
+    public members: Member[];
+    constructor(private http: Http) {
+    }
+
+    public ngOnInit(): void {
+        this.http.get('http://localhost:50405/members')
+            .map((res: Response) => res.json().results)
+            .do(res => console.log(res))
+            .subscribe(members => this.members = members);
+    }
 }
